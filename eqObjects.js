9b53737b2d8afe1e35f0e76e0a207b8b506981b4
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-08-24 21:32:51 +0000

    <comitting this line of code>

--- eqObjects.js
+++ eqObjects.js
@@ -44,7 +44,7 @@ const assertEqual = function(actual, expected) {
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+console.log(eqObjects(ab, ba)); 
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+console.log(eqObjects(ab, abc)); 
