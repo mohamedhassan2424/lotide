ed4d223687e62d314de599ec28bc1ac86b644119
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-08-24 14:02:58 +0000

    <comiiting this code>

---
+++ assertArrayEqual.js
@@ -0,0 +1,16 @@
+const eqArrays= function(arrayValue1,arrayValue2){
+    if(arrayValue1.length !==arrayValue2.length){
+        return false;
+    }
+    for(let i=0; i<arrayValue1.length;i++){
+        if(Array.isArray(arrayValue1[i]) &&(eqArrays(arrayValue1[i],arrayValue2[i]) === false)){
+        return false;
+    }
+    if(!(Array.isArray(arrayValue1[i]))&& arrayValue1[i] !==arrayValue2[i]){
+        return false
+    }
+}
+    return true;
+};
+console.log(eqArrays([1, 2,3], [1, 2, 3]));
+console.log(eqArrays([13, 4,4], [14, 6, 4]));
